2b438c3b24e12feaf2a805ebb629961bc79e96d6
Carl Sargunar <carl@sargunar.com>
2024-10-21 23:59:30 +0100

    dbContext

---
+++ Docker Demo/DemoLib/Data/MessageDBContext.cs
@@ -0,0 +1,19 @@
+﻿using DemoLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoLib.Data
+{
+    public class MessageDbContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+
+        public MessageDbContext(DbContextOptions<MessageDbContext> options) : base(options)
+        {
+        }
+    }
+}
