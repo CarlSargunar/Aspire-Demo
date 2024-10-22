677719891c1e27debe7c482e7b7520abb2bd77a4
Carl Sargunar <carl@sargunar.com>
2024-10-22 20:09:29 +0100

    getting closer

---
+++ Docker Demo/DemoLib/Config/RmqConfig.cs
@@ -0,0 +1,7 @@
+namespace DemoLib.Config;
+public class RmqConfig
+{
+    public string HostName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
--- Docker Demo/MessageProcessor/Workers/Processor.cs
+++ Docker Demo/MessageProcessor/Workers/Processor.cs
@@ -3,6 +3,7 @@ using DemoLib.Models;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using MessageProcessor.Data;
+using DemoLib.Config;
 
 namespace MessageProcessor.Workers;
 public class Processor : BackgroundService
@@ -10,20 +11,30 @@ public class Processor : BackgroundService
     private readonly ILogger<Processor> _logger;
     private readonly IServiceScopeFactory _scopeFactory;
     private IConnection _connection;
+    private IConfiguration _configuration;
     private IModel _channel;
 
-    public Processor(ILogger<Processor> logger, IServiceScopeFactory scopeFactory)
+    public Processor(ILogger<Processor> logger, IServiceScopeFactory scopeFactory, IConfiguration configuration)
     {
         _logger = logger;
         _scopeFactory = scopeFactory;
+        _configuration = configuration;
+
+        var rabbitMqConfig = _configuration.GetSection("RMQConfig").Get<RmqConfig>();
+        if (rabbitMqConfig == null)
+        {
+            throw new ArgumentNullException("Rabbit MQ Config not set");
+        }
+        _logger.LogInformation("RabbitMQ Config: {0}", rabbitMqConfig.HostName);
+
 
         var factory = new ConnectionFactory()
         {
-            HostName = "localhost",
-            UserName = "guest",
-            Password = "guest"
-        }; 
-        
+            HostName = rabbitMqConfig.HostName,
+            UserName = rabbitMqConfig.UserName,
+            Password = rabbitMqConfig.Password
+        };
+
         _connection = factory.CreateConnection();
         _channel = _connection.CreateModel();
         _channel.QueueDeclare(queue: "demo-message-queue",
@@ -42,22 +53,33 @@ public class Processor : BackgroundService
         var consumer = new EventingBasicConsumer(_channel);
         consumer.Received += async (model, ea) =>
         {
-            _logger.LogInformation("Message received from RabbitMQ.");
-            var body = ea.Body.ToArray();
-            var message = System.Text.Json.JsonSerializer.Deserialize<ServiceMessage>(body);
+            try
+            {
+                _logger.LogInformation("Message received from RabbitMQ.");
+                var body = ea.Body.ToArray();
+                var message = System.Text.Json.JsonSerializer.Deserialize<ServiceMessage>(body);
 
-            _logger.LogInformation("Received message: {0}", message?.MessageBody);
+                _logger.LogInformation("Received message: {0}", message?.MessageBody);
 
-            // Process the message and write to the database
-            await ProcessMessageAsync(message);
+                // Process the message and write to the database
+                await ProcessMessageAsync(message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing message.");
+            }
         };
 
         _channel.BasicConsume(queue: "demo-message-queue", autoAck: true, consumer: consumer);
 
         // Keep the service alive
+        int i = 0;
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Processor running at: {time}", DateTimeOffset.Now);
+            if (i % 10 == 0)
+            {
+                _logger.LogInformation("Processor running at: {time}", DateTimeOffset.Now);
+            }
             await Task.Delay(1000, stoppingToken);
         }
     }
