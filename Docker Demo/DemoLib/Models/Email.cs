dc0db43e47b5b9be1ed41ad3dc1932b4815d35f2
Carl Sargunar <carl@sargunar.com>
2024-10-22 12:33:33 +0100

    db

--- Docker Demo/DemoLib/Models/Email.cs
+++ Docker Demo/DemoLib/Models/Email.cs
@@ -3,7 +3,7 @@ namespace DemoLib.Models;
 public class Email
 {
     public int Id { get; set; }
-    public int MessageId { get; set; }
+    public Guid MessageId { get; set; }
     public string EmailAddress { get; set; }
     public string Name { get; set; }
     public string Message { get; set; }
