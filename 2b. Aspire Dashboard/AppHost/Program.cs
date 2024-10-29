0eb051be6129671cdcc94e9f0207e304c87a242f
Carl Sargunar <carl@sargunar.com>
2024-10-29 18:43:14 +0000

    blazor

--- 2b. Aspire Dashboard/AppHost/Program.cs
+++ 2b. Aspire Dashboard/AppHost/Program.cs
@@ -2,5 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var api = builder.AddProject<Projects.WeatherApi>("api");
+
+var app = builder.AddProject<Projects.AspireApp>("app");
 
 builder.Build().Run();
