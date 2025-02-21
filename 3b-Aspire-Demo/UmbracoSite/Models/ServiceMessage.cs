bf280b07928e426a4a5b1df0eaabe841459316e4
Carl Sargunar <carl@sargunar.com>
2025-02-21 16:17:48 +0000

    Add comment for issue

--- 3b-Aspire-Demo/UmbracoSite/Models/ServiceMessage.cs
+++ 3b-Aspire-Demo/UmbracoSite/Models/ServiceMessage.cs
@@ -2,6 +2,8 @@
 
 namespace DemoLib.Models;
 
+// Duplicate of DemoLib.Models.ServiceMessage - becuase if I reference the DemoLib project, throws an error
+// System.TypeLoadException: 'Method 'get_LockReleaseBehavior' in type 'Microsoft.EntityFrameworkCore.Sqlite.Migrations.Internal.SqliteHistoryRepository' from assembly 'Microsoft.EntityFrameworkCore.Sqlite, Version=8.0.11.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' does not have an implementation.'
 public class ServiceMessage
 {
     public int Id { get; set; }
