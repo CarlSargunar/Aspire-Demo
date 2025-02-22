8276b16f456ad75854457c77a656e2a53982017e
Carl Sargunar <carl@sargunar.com>
2025-02-22 04:01:57 +0000

    App Host for Full Orchestration

---
+++ 2b-Aspire-Demo-v9/Demo Files/Program.cs
@@ -0,0 +1,30 @@
+using Aspire.Hosting;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var sqlPassword = builder.AddParameter("sql-password");
+var sqlContainer = builder.AddSqlServer("sql", sqlPassword, 1433)
+    .AddDatabase("reportingdb");
+
+var rmqpassword = builder.AddParameter("messaging-password", secret: true);
+var rabbitMq = builder.AddRabbitMQ("messaging", password: rmqpassword)
+    .WithManagementPlugin();
+
+var umbraco = builder.AddProject<Projects.UmbracoSite>("umbwebsite")
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq);
+
+var processor = builder.AddProject<Projects.MessageProcessor>("processor")
+    .WithReference(sqlContainer)
+    .WaitFor(sqlContainer)
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq);
+
+var analyticsapi = builder.AddProject<Projects.DemoApi>("analyticsapi")
+    .WithReference(sqlContainer)
+    .WaitFor(sqlContainer);
+
+var analytics = builder.AddProject<Projects.AnalyticsApp>("analyticsapp")
+    .WithReference(analyticsapi);
+
+builder.Build().Run();
